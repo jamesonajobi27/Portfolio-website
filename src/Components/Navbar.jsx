e5a47d0ab66a35a322df8aea6e6ef3505ed8c05f
Onajobi James <104567360+jamesonajobi27@users.noreply.github.com>
2026-02-18 10:26:44 -0330

    Redesign portfolio into engineering-focused landing experience

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
-export default function Navbar(){
-    return(
-        <div className="Navbar">
-          <Link to="/" className="link">Home</Link>
-          <Link to="/Repositories" className="link">Repositories</Link>
-          <Link className="link" to="/TestingError">Testing Error</Link>
-        </div>
-    )
+export default function Navbar() {
+  return (
+    <header className="nav-shell">
+      <div className="brand">James Onajobi</div>
+      <nav className="Navbar">
+        <Link to="/" className="link">Home</Link>
+        <Link to="/Repositories" className="link">Projects</Link>
+        <a className="link" href="/James_Onajobi_Resume.pdf" target="_blank" rel="noreferrer">Resume</a>
+        <a className="link" href="https://github.com/jamesonajobi27" target="_blank" rel="noreferrer">GitHub</a>
+      </nav>
+    </header>
+  );
 }
